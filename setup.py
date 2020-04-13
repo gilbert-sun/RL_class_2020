054d8846ec4eb3b65cca3866e1c831cb4a0de6e2
gilbertsun <gilbertsun@itri.org.tw>
2020-04-13 13:58:44 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RL_class_2020',
+    version='1.0',
+    packages=[''],
+    url='GPL v2.0',
+    license='ITRI',
+    author='root',
+    author_email='gilbertsun@itri.org.tw',
+    description='NCTU 2020 RL homework'
+)
